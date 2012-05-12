d915bc29b0bc58cd01d907bbe3a6615a5f8bf8cc
Sebastian Widmer <widmer.sebastian@gmail.com>
2012-05-12 11:43:40 +0200

    new folder for modules

---
+++ modules/game.js
@@ -0,0 +1,35 @@
+﻿/*
+*	wikiway
+*	Authors: Andrin Schnegg (andrin[at]schnegg.biz),
+*			 Sebastian Widmer (widmer.sebastian[at]gmail.com)
+*	Version: Experimental
+*	Datum:   11.05.2012
+*/
+
+//Array where all the Games are saved
+var games = new Array();
+
+//Make New Game
+exports.createGame = function(startArticle, endActicle){
+	return "";
+};
+
+//Join Game
+exports.joinGame = function(gameId){
+	return 0;
+};
+
+//List all Games
+exports.listGames = function(){
+	return "Liste der Games...";
+};
+
+//Checks if user is in Game
+exports.checkUser = function(client){
+	return 0;
+};
+
+//Next Step for User
+exports.checkUser = function(client){
+	return 0;
+};
---
+++ modules/templ.js
@@ -0,0 +1,28 @@
+/*
+*	wikiway
+*	Authors: Andrin Schnegg (andrin[at]schnegg.biz),
+*			 Sebastian Widmer (widmer.sebastian[at]gmail.com)
+*	Version: Experimental
+*	Datum:   12.05.2012
+*/
+//Use Jade and node.js Filesystem
+var jade = require('jade'),
+	fs 	= require('fs');
+
+//Render stuff
+exports.render = function(file, locals, callback){
+	//Make nice filepath
+	file = './views/'+file+'.jade';
+	
+	//Read file async
+	fs.readFile(file, 'utf8', function (err, data){
+		//throw error
+		if (err) throw err;
+		//Compile template
+		layout = jade.compile(data, { filename: file, locals: locals });
+		//Call callback with compiled layout
+		if (callback != null){
+			callback(layout());
+		};
+	});
+};
