d422192dae3d37a6705b75b71218141939168703
Sebastian Widmer <widmer.sebastian@gmail.com>
2012-06-02 15:40:33 +0200

    don't kill server if there is no input into escape/encode functions

--- modules/tools.js
+++ modules/tools.js
@@ -6,13 +6,16 @@
 *	Datum:   22.05.2012
 */
 exports.uriEncode = function (unencoded) {
+	if (unencoded == null) return null;
 	return encodeURIComponent(unencoded.replace(/ /g,'_'));
 }
 
 exports.uriDecode = function (encoded) {
+	if (encoded == null) return null;
 	return decodeURIComponent(encoded.replace(/_/g, ' '));
 }
 
 exports.escape = function(unescaped) {
+	if (unescaped == null) return null;
 	return unescaped.replace(/&/g,"&amp;").replace(/</g,"&lt;").replace(/>/g,"&gt;");
 };
